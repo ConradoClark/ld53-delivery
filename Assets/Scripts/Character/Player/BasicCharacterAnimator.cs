c0a31619214e17f22b47119f4a377f02ca8ab43a
ConradoClarke <conradoclarke@gmail.com>
2023-04-30 10:28:30 -0300

    Improved Character

---
+++ Assets/Scripts/Character/Player/BasicCharacterAnimator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Licht.Unity.CharacterControllers;
+using Licht.Unity.Objects;
+using UnityEngine;
+
+public class BasicCharacterAnimator : BaseGameObject
+{
+    [field:SerializeField]
+    public SpriteRenderer MainSpriteRenderer { get; private set; }
+
+    [field: SerializeField]
+    public LichtTopDownMoveController MoveController { get; private set; }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+    }
+
+    private void Update()
+    {
+        if (MoveController.LatestDirection.x == 0) return;
+
+        MainSpriteRenderer.flipX = MoveController.LatestDirection.x < 0;
+    }
+}
