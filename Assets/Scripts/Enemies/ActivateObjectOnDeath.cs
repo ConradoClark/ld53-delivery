e30aceae66db242be7008412b3298c393d8861d8
ConradoClarke <conradoclarke@gmail.com>
2023-05-01 17:13:43 -0300

    Is this the end? WOW

---
+++ Assets/Scripts/Enemies/ActivateObjectOnDeath.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Licht.Unity.Objects;
+using UnityEngine;
+using Random = System.Random;
+
+internal class ActivateObjectOnDeath : BaseGameObject
+{
+    [field: SerializeField]
+    public EnemyDieWhenZeroHP EnemyDeath { get; private set; }
+
+    [field: SerializeField]
+    public GameObject Target { get; private set; }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        EnemyDeath.OnDeath += EnemyDeath_OnDeath;
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+        EnemyDeath.OnDeath -= EnemyDeath_OnDeath;
+    }
+
+    private void EnemyDeath_OnDeath()
+    {
+        Target.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Enemies/SpawnEnemyPeriodically.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Licht.Impl.Orchestration;
+using Licht.Unity.Extensions;
+using Licht.Unity.Objects;
+using UnityEngine;
+
+public class SpawnEnemyPeriodically : BaseGameRunner
+{
+    private EnemyPoolManager _enemyPoolManager;
+
+    [field: SerializeField]
+    public RoomObject RoomObject { get; private set; }
+
+    [field:SerializeField]
+    public ScriptPrefab EnemyPrefab { get; private set; }
+
+    [field: SerializeField]
+    public float DelayInSeconds { get; private set; }
+
+    protected override void OnAwake()
+    {
+        base.OnAwake();
+        _enemyPoolManager = _enemyPoolManager.FromScene(true);
+    }
+    protected override IEnumerable<IEnumerable<Action>> Handle()
+    {
+        yield return TimeYields.WaitSeconds(GameTimer, DelayInSeconds);
+        _enemyPoolManager.GetEffect(EnemyPrefab).TryGetFromPool(out _, e =>
+        {
+            e.transform.position = transform.position;
+            e.RoomObject.SetRoomInstance(RoomObject.Room);
+            e.Randomize();
+        });
+    }
+
+
+}
